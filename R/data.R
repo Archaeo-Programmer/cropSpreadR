31d92b96038de95a609affd1aaa4d981246201c3
Andrew Gillreath-Brown <andrew.brown1234@gmail.com>
2022-10-04 23:10:26 -0700

    updated documentation.

--- R/data.R
+++ R/data.R
@@ -1,32 +1,33 @@
-#' Annual Accumulated Growing Degree Days from PRISM.
+#' Annual Accumulated Growing Degree Days from PRISM
 #'
-#' A `raster` containing annual accumulated growing degree days with a base temperature of 10°C and maximum temperature of 30°C.
+#' @description A `raster` containing annual accumulated growing degree days with a base temperature of 10°C and maximum temperature of 30°C.
 #' GDD was calculated from tmin and max 30 seconds (~1 \ifelse{html}{\out{km<sup>2</sup>}}{\eqn{km^2}}).
 #'
 #' @format An object of class `RasterLayer`.
 #' @source \url{https://www.worldclim.org/data/worldclim21.html}
 "PRISM_annual_gdd"
 
-#' Growing Season Accumulated Growing Degree Days from PRISM.
+#' Growing Season Accumulated Growing Degree Days from PRISM
 #'
-#' A `raster` containing accumulated growing degree days for the growing season (May through September) with a base temperature of 10°C and maximum temperature of 30°C.
+#' @description A `raster` containing accumulated growing degree days for the growing season (May through September) with a base temperature of 10°C and maximum temperature of 30°C.
 #' GDD was calculated from tmin and max 30 seconds (~1 \ifelse{html}{\out{km<sup>2</sup>}}{\eqn{km^2}}).
 #'
 #' @format An object of class `RasterLayer`.
-#' @source \url{https://www.worldclim.org/data/worldclim21.html}
+#' @source \url{http://www.prism.oregonstate.edu/}
 "PRISM_gs_gdd"
 
-#' Four Corners County Shapefiles.
+#' Four Corners County Shapefiles
 #'
-#' A `sf` `data.frame` containing county shapefiles for Arizona, Colorado, New Mexico, and Utah.
+#' @description An `sf` `data.frame` containing county polygons for Arizona, Colorado, New Mexico, and Utah.
 #'
 #' @format An object of class `sfc_POLYGON`.
 #' @source \url{https://github.com/adeckmyn/maps}
 "SWUS_counties"
 
-#' Radiocarbon Data for Four Corners.
+#' Radiocarbon Data for Four Corners
 #'
-#' A `data.frame` containing radiocarbon data for the Four Corners. Data was combined from `p3k14c` with `cropDiffusionR`.
+#' @description A `data.frame` containing radiocarbon data for the Four Corners. `p3k14c` data was combined with `cropDiffusionR`.
+#' @format An object of class `tbl_df` with the following variables:
 #' \describe{
 #' \item{\code{LabID}}{Lab ID of each radiocarbon date}
 #' \item{\code{SiteID}}{ID of the site from which the sample has been recovered}
@@ -39,15 +40,21 @@
 #' \item{\code{Error}}{Radiocarbon age error}
 #' \item{\code{calib}}{Calibration curve to be used to calibrate the Age}
 #'}
-#'
-#' @format An object of class `tbl_df`.
 #' @source \url{https://github.com/people3k/p3k14c}
-#' @source \url{https://github.com/Archaeo-Programmer/cropDiffusionR/blob/main/vignettes/tables/tableS1_with_references.xlsx}
+#' @source \url{https://github.com/Archaeo-Programmer/cropDiffusionR/blob/main/vignettes/tables}
 "sw_rc"
 
-#' Summed Probability Distributions for Four Corners Counties.
+#' Summed Probability Distributions for Four Corners Counties
 #'
-#' A `tibble` containing SPDs for counties in Arizona, Colorado, New Mexico, and Utah. Also, contains the number of samples and original data used to make the SPD.
+#' @description A `tibble` containing SPDs for counties in Arizona, Colorado, New Mexico, and Utah. Also, contains the number of samples and original data used to make the SPD.
 #'
 #' @format An object of class `tbl_df`.
+#' \describe{
+#' \item{\code{NAME}}{County of the site from which the sample has been recovered}
+#' \item{\code{REGION}}{State of the site from which the sample has been recovered}
+#' \item{\code{n}}{Sample size of radiocarbon dates for each county}
+#' \item{\code{data}}{Radiocarbon samples in each county as a nested `tibble`}
+#' \item{\code{spds}}{SPDs for each county as an object of class `CalGrid`}
+#' \item{\code{county_st}}{County and state of the site from which the sample has been recovered}
+#'}
 "SPD_county"
